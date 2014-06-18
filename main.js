355c28057367d6508f308eb4e56b3f084fe8e84f
Surya <n.suryap@gmail.com>
2014-06-18 10:56:17 +0530

    Fixed newline not working in windows

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-// --------------------------------------------------Change these variables for ur needs ----------------------------------------------.
-var def = 106112000;    					  //base roll_no of class                                                                  |
-var nameOfFile = def+'';					  // <Ur desired name of file to be written > default is (rollnoseries)                    |
-/*																																	   |
-	Base Roll Nos of classes                                                                    									   |
-																																	   |
-    1061xx000 :- Computer Science n Engineering																						   |
-	1031xx000 :- Civil																												   |
-	1101xx000 :- ICE																												   |
-	1071xx000 :- EEE   																												   |
-	1081xx000 :- ECE																												   |
- 																																	   |
----------------------------------------------------------------------------------------------------------------------------------------'
-*/
-
-
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
@@ -25,15 +9,39 @@ var postData;
 var vwre = /name=\"__VIEWSTATE\" value=\"(.+)\"/;
 var viewstate;
 var masterList = [];
-var wstream = fs.createWriteStream(nameOfFile);
 
+//____________________________________________________________________________________________________________________
+//-                                    ____                         ____     _____
+//                                    /       |   |    /\    |\  | /        |
+//                                   |        |---|   /__\   | \ | |   __.  |__
+//                                    \____   |   |  /    \  |  \|  \____|  |______
+//
+//
+//													|   |   __          __
+//													|---|  (__)  |---  (__)
+//													|	|   \__  |      \__
+//
+//
+//										DefaultRollNo must be rollNo of the first person in your batch in your class
 
-var callback = function(){
-	console.log("Over");
+							var DefaultRollNo = 107112001;
+
+// ____________________________________________________________________________________________________________________
+
+var departs = ['Archi','Chem','Civil','','','CSE','EEE','ECE','','ICE','Mech','Meta','','Prod'];
+var nameOfFile = '';
+var branch = parseInt((DefaultRollNo+'').substring(1,3));
+if(branch<15){
+	nameOfFile+=departs[branch-1];
+}else{
+	nameOfFile+=(branch-1);
 }
 
-function findVs(postData,callback){
-	console.log("findVs called");
+var year = parseInt((DefaultRollNo+'').substring(4,6));
+var endYear = year+4;
+nameOfFile+= ('_'+year + '-' + endYear +'.txt');
+var wstream = fs.createWriteStream(nameOfFile);
+function findVs(postData){
 	var cookieJar = request.jar();
 	request.post({
     uri:url,
@@ -42,51 +50,55 @@ function findVs(postData,callback){
     body:require('querystring').stringify(postData)
     },function(err,res,body){
     	$ = cheerio.load(body);
-       console.log(body);
-        var Vs = body.match(vwre)[1];
-        var postData2;
+       
+       var Vs = body.match(vwre)[1];
+       
+       
+		var postData2;
 		
-	    for (var i = 1; i < 120; i++) {
-			rollno = def+i+'';                  // 106112000 is computer science... 
+		for (var i = 1; i < 120; i++) {
+			rollno = DefaultRollNo+i+'';                  // 106112000 is computer science... 
 			postData2 = {
-				__EVENTTARGET: 'Dt1',
-				__EVENTARGUMENT: '',
-				__VIEWSTATE:Vs,
-				TextBox1:rollno,
-				Dt1:'96'
-			};
+			__EVENTTARGET: 'Dt1',
+			__EVENTARGUMENT: '',
+			__VIEWSTATE:Vs,
+			TextBox1:rollno,
+			Dt1:'96'
+		};
 		
-        	request.post({
-       			uri : url,
-       			jar : cookieJar,
-       			headers:{'content-type': 'application/x-www-form-urlencoded'},
-    			body:require('querystring').stringify(postData2)
-    		},function(err,resp,body){
+       request.post({
+       		uri : url,
+       		jar : cookieJar,
+       		headers:{'content-type': 'application/x-www-form-urlencoded'},
+    		body:require('querystring').stringify(postData2)
+    	},function(err,resp,body){
 
-    			var person = [];
-    			$ = cheerio.load(body);
-    			person["Name"] = $("#LblName").text();
-    		
-    			person["GPA"] = $("#LblGPA").text();
-    			if(person["Name"].length != 0){
-    				while(person["Name"].length < 35){
-    					person["Name"]+=' ';
-    				}
-    				masterList.push(person);
-    				var t2w = $("#LblEnrollmentNo").text()+ '. ' + person["Name"]+' '+person["GPA"];
-    				wstream.write(t2w + '\n');
-    				console.log(person["Name"]);
-    			}
+    		var person = [];
+    		$ = cheerio.load(body);
+    		person["Name"] = $("#LblName").text();
+    		person["Roll"] =  $("#LblEnrollmentNo").text();
+    		person["GPA"] = $("#LblGPA").text();
+    		if(person["Name"].length != 0){
+    			while(person['Name'].length !=35) person['Name']+=' ';
+    			 masterList.push(person);
+    			var t2w =person["Roll"]+ '. ' + person["Name"]+' '+person["GPA"];
+    			wstream.write(t2w + '\r\n' + '\n');
+    		}
 
-    		});
+    	}
+       );
+
+
 
 		};
+
+		
+		 
+    
 	});
-	
 }
-console.log("Started for " + def);
-console.log("Wait till the output file contains the desired results (depends in your internet connection) ");
-function getResults(callback){
+
+function getResults(){
 	request(url,function(err,resp,body){
 		if(err) throw err;
 		$ = cheerio.load(body);
@@ -95,17 +107,14 @@ function getResults(callback){
 		postData = {
 
 			__VIEWSTATE:viewstate,
-			TextBox1:def + '',
+			TextBox1:DefaultRollNo + '',
 			Button1:'Show'
 		};
-		findVs(postData,function(){
-			callback();
-			});
+		findVs(postData);
 	
 	});
-
 }
-getResults(callback);
+getResults();
 
 
 
